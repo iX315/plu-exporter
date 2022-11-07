0952a476ec299e25e59173b11ac324f717b0e70a
Jury Giannelli <iX315@me.com>
2022-11-07 16:00:50 +0000

    first commit and maybe last... nah ;D

---
+++ pages/api/data.ts
@@ -0,0 +1,72 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export type MenuData = {
+  group: GroupData
+  products: ProductData[]
+}
+
+export type GroupData = {
+  Name: string
+  Description: string
+  pre: string
+  post: string
+}
+
+export type ProductData = {
+  PLU: number
+  Group: string
+  Name: string
+  Allergies: string
+  Details: string
+  Description: string
+  Size: string
+  Price: number
+  Page: number
+}
+
+export type Data = {
+  values: MenuData[]
+}
+
+const SheetDB_Api_Call = (params: any = {}) => {
+  const envUser = process.env.SHEETDB_API_USERNAME ?? ''
+  const envPass = process.env.SHEETDB_API_PASSWORD ?? ''
+  let envUrl = process.env.SHEETDB_API_URL ?? ''
+
+  const options: RequestInit = {
+    headers: {
+      Authorization: 'Basic ' + Buffer.from(envUser + ':' + envPass).toString('base64')
+    }
+  }
+
+  if (params) {
+    const urlParams = new URLSearchParams(params).toString()
+    envUrl = envUrl + '?' + urlParams
+  }
+  return fetch(envUrl, options)
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const productsCall = await SheetDB_Api_Call()
+  const groupsCall = await SheetDB_Api_Call({ "sheet": "Groups" })
+
+  if (productsCall.ok && groupsCall.ok) {
+    const products = await productsCall.json() as ProductData[]
+    const groups = await groupsCall.json() as GroupData[]
+
+    const menuData: MenuData[] = groups.map((group) => (
+      {
+        group,
+        products: products.filter(product => product.Group === group.Name)
+      }
+    ))
+
+    res.status(200).json({ values: menuData })
+  } else {
+    res.status(500).json({ values: [] })
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import { Box, Grid, Text } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import { Data, ProductData } from "./api/data"
+
+const ProductRow = ({ PLU, Name, Allergies, Details, Description, Size, Price }: ProductData) => (
+  <Grid templateColumns='10% 1fr 10% 10%' gap={2} pb={"0.5em"} className={'avoidBreakPageInside'}>
+    <Text>{PLU}</Text>
+    <Box>
+      <Text as="b">{Name}</Text>{Allergies && ' '}<Text as='sup'>{Allergies}</Text>{Details && ' '}<Text as="i">{Details}</Text>
+      <br />
+      <Text size="sm"><i>{Description}</i></Text>
+    </Box>
+    <Text>{Size}</Text>
+    <Text>{Price}</Text>
+  </Grid>
+)
+
+export default function Home() {
+  const [data, setData] = useState<Data>()
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('/api/data')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data || !data.values) return <p>No data</p>
+
+  return (
+    <Box padding={"3em"}>
+      {data.values.map(({ group, products }, i) => {
+        return (
+          <Box key={i} pb={"2em"} className={'avoidBreakPageInside'}>
+            <Text>{group.pre}</Text>
+            <Text as="h2" color="orange"><b><i>{group.Name}</i></b></Text>
+            <Text as="h3" color="orange" size="md" py={"0.2em"}><i>{group.Description}</i></Text>
+            <Box py={"1em"}>
+              {products.map((product, i) => <ProductRow key={i} {...product} />)}
+            </Box>
+            <Text align={"center"} size="md"><b><i>{group.post}</i></b></Text>
+          </Box>
+        )
+      })}
+    </Box>
+  )
+}
