8b7ec8e2319977fd9cf9ba7dee972965c0f385c0
Jury Giannelli <jury.giannelli@sdox.io>
2024-05-12 10:10:46 +0200

    fix clear cache

--- src/app/api/clearCache/route.ts
+++ src/app/api/clearCache/route.ts
@@ -2,33 +2,25 @@
 import { isCorrectSecret, isProd } from "../../../utils/env"
 import { NextResponse } from "next/server"
 
-export async function ALL(req: Request) {
+const handler = async (req: Request) => {
   const { secret } = await req.json()
 
   if (!process.env.CACHE_SECRET || !secret || Array.isArray(secret)) {
-    return NextResponse.json(
-      { message: "Not available" },
-      {
-        status: 401,
-      },
-    )
+    return NextResponse.json({ message: "Not available" }, { status: 401 })
     return
   }
 
   const methodIsCorrect = req.method === (isProd() ? "POST" : "GET")
 
   if (isCorrectSecret(secret) && methodIsCorrect) {
-    return NextResponse.json(
-      { message: "Cache cleared" },
-      {
-        status: 200,
-      },
-    )
+    return NextResponse.json({ message: "Cache cleared" }, { status: 200 })
   }
-  return NextResponse.json(
-    { message: "Not allowed" },
-    {
-      status: 500,
-    },
-  )
+  return NextResponse.json({ message: "Forbidden" }, { status: 403 })
+}
+
+export async function GET(req: Request) {
+  handler(req)
+}
+export async function POST(req: Request) {
+  handler(req)
 }
