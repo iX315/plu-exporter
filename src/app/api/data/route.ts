0bee5850519b5f23526230f65390fdcf21228d1d
Jury Giannelli <jury.giannelli@sdox.io>
2024-05-12 10:11:00 +0200

    refactor data route api

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -3,29 +3,7 @@ import { NextResponse } from "next/server"
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { GroupData, MenuData, ProductData } from "../../../types"
-import { GoogleSheetsApiCall } from "../../../utils/Api"
-
-const parseProduct = (product: string[]) =>
-  ({
-    PLU: product[0] ? Number(product[0]) : null,
-    Group: product[1],
-    Name: product[2],
-    Allergies: product[3],
-    Details: product[4],
-    Description: product[5],
-    Size: product[6],
-    Price: product[7],
-    Page: Number(product[8]),
-  }) as ProductData
-
-const parseGroup = (group: string[]) =>
-  ({
-    Name: group[0],
-    Description: group[1],
-    pre: group[2],
-    post: group[3],
-    page: Number(group[4]),
-  }) as GroupData
+import { GoogleSheetsApiCall, parseGroup, parseProduct } from "../../../utils/Api"
 
 export async function GET() {
   const menuApiResponse = await GoogleSheetsApiCall()
--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,8 +1,31 @@
 import { readFile, writeFile } from "fs/promises"
 import { google } from "googleapis"
+import { GroupData, ProductData } from "../types"
 
 // tmp folder is the only writable folder in vercel
 const keyFilePath = "./tmp/credentials.json"
+
+export const parseProduct = (product: string[]) =>
+  ({
+    PLU: product[0] ? Number(product[0]) : null,
+    Group: product[1],
+    Name: product[2],
+    Allergies: product[3],
+    Details: product[4],
+    Description: product[5],
+    Size: product[6],
+    Price: product[7],
+    Page: Number(product[8]),
+  }) as ProductData
+
+export const parseGroup = (group: string[]) =>
+  ({
+    Name: group[0],
+    Description: group[1],
+    pre: group[2],
+    post: group[3],
+    page: Number(group[4]),
+  }) as GroupData
 
 const createTempKeyFile = async () => {
   const data = Buffer.from(process.env.CREDENTIALS ?? "", "base64")
