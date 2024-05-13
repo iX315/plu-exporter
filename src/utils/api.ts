8daa9a571ebd499a9677781a706b52c9212deb0a
Jury Giannelli <jury.giannelli@sdox.io>
2024-05-13 05:20:21 +0200

    remove cache file read credentials directly from env instead of writing temp file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,9 +3,6 @@ import { google } from "googleapis"
 import { GroupData, MenuData, ProductData } from "@/types"
 import path from "path"
 
-// tmp folder is the only writable folder in vercel
-const keyFilePath = path.join(process.cwd(), '/tmp/credentials.json')
-
 export const parseProduct = (product: string[]) =>
   ({
     PLU: product[0] ? Number(product[0]) : null,
@@ -28,45 +25,16 @@ export const parseGroup = (group: string[]) =>
     page: Number(group[4]),
   }) as GroupData
 
-const createTempKeyFile = async () => {
-  const data = Buffer.from(process.env.CREDENTIALS ?? "", "base64")
-    .toString()
-    .replace(/\n/g, "") //  hack to remove new lines
-
-  // write credentials to file
-  try {
-    await writeFile(keyFilePath, data)
-    return true
-  } catch (err) {
-    console.error(err)
-    return false
-  }
-}
+const getCredentials = () => JSON.parse(Buffer.from(process.env.CREDENTIALS ?? "", "base64").toString())
 
 export const GoogleSheetsApiCall = async ({
   sheetName = "Menu",
   startRange = "A2",
   endRange = "Z14989",
 } = {}) => {
-  const cacheDataPath = path.join(process.cwd(), `/tmp/cache/${sheetName}${startRange}${endRange}.json`)
-
-  // return cached data from file
-  try {
-    const data = await readFile(cacheDataPath)
-    return JSON.parse(data.toString())
-  } catch (error) {
-    console.log("GoogleSheetsApiCall: No cache data found")
-  }
-
-  const keyCreated = await createTempKeyFile()
-
-  if (!keyCreated) {
-    throw new Error("Could not create credentials file")
-  }
-
   const auth = await google.auth.getClient({
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-    keyFile: keyFilePath,
+    credentials: getCredentials(),
   })
 
   const sheets = google.sheets({ version: "v4", auth })
@@ -80,9 +48,7 @@ export const GoogleSheetsApiCall = async ({
     majorDimension: "ROWS",
   })
 
-  writeFile(cacheDataPath, JSON.stringify(response.data.values))
-
-  return response.data.values
+  return response.data.values ?? []
 }
 
 export const GetMenuData = async () => {
