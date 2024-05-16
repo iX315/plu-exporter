f863a7bd5fc131fb2673c7fe88177eb0e54ed6c3
Jury Giannelli <jury.giannelli@sdox.io>
2024-05-16 13:52:12 +0200

    rework api separate parsers

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,29 +1,6 @@
-import { readFile, writeFile } from "fs/promises"
 import { google } from "googleapis"
-import { GroupData, MenuData, ProductData } from "@/types"
-import path from "path"
-
-export const parseProduct = (product: string[]) =>
-  ({
-    PLU: product[0] ? Number(product[0]) : null,
-    Group: product[1],
-    Name: product[2],
-    Allergies: product[3],
-    Details: product[4],
-    Description: product[5],
-    Size: product[6],
-    Price: product[7],
-    Page: Number(product[8]),
-  }) as ProductData
-
-export const parseGroup = (group: string[]) =>
-  ({
-    Name: group[0],
-    Description: group[1],
-    pre: group[2],
-    post: group[3],
-    page: Number(group[4]),
-  }) as GroupData
+import { MenuData } from "@/types"
+import { parseGroup, parseProduct } from "./parsers"
 
 const getCredentials = () => JSON.parse(Buffer.from(process.env.CREDENTIALS ?? "", "base64").toString())
 
@@ -51,16 +28,21 @@ export const GoogleSheetsApiCall = async ({
   return response.data.values ?? []
 }
 
-export const GetMenuData = async () => {
+export const getMenu = async () => {
   const menuApiResponse = await GoogleSheetsApiCall()
+  return menuApiResponse.map(parseProduct)
+}
+
+export const getGroups = async () => {
   const groupsApiResponse = await GoogleSheetsApiCall({ sheetName: "Groups" })
-  const arraysAreValid =
-    Array.isArray(menuApiResponse) && Array.isArray(groupsApiResponse)
+  return groupsApiResponse.map(parseGroup)
+}
 
-  const products: ProductData[] = menuApiResponse.map(parseProduct)
-  const groups: GroupData[] = groupsApiResponse.map(parseGroup)
+export const getMenuData = async () => {
+  const products = await getMenu() ?? []
+  const groups = await getGroups() ?? []
 
-  if (arraysAreValid) {
+  if (Array.isArray(products) && Array.isArray(groups)) {
     const menuData: MenuData[] = groups.map((group) => ({
       group,
       products: products.filter((product) => product.Group === group.Name),
