0e3b5e7b346e5a80b4e8d9c55dab4f494ef4a5e4
Jury Giannelli <jury.giannelli@sdox.io>
2024-05-17 12:15:05 +0200

    migrate main page to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Box, Text } from "@chakra-ui/react"
 import { getPagesData } from "@/models/Page"
 import { isDev } from "@/utils"
+import Image from "next/image"
 import Link from "next/link"
 
 export const revalidate = isDev() ? 0 : 3600
@@ -8,28 +8,28 @@ export const revalidate = isDev() ? 0 : 3600
 export default async function Home() {
   const values = (await getPagesData()).find(({id}) => id === "Root")
 
-  if (!values) return <Box>500...</Box>
+  if (!values) return <div>500...</div>
 
   return (
-    <Box mr={["auto", "300px"]}>
+    <div className="mr-auto lg:mr-[300px]">
       {values.image ? (
-        <Box
-          mx={"auto"}
-          maxW={"980px"}
-          bgImage={values.image}
-          aspectRatio={"3/1"}
-          bgSize={"cover"}
+        <Image
+          src={values.image}
+          width={896}
+          height={288}
+          className="mx-auto max-w-4xl aspect-[3/1] object-cover"
+          alt="Cover image"
         />
       ) : null}
-      <Box mx={"auto"} p={"3em"} maxW={"980px"} bgColor={"#f3f3f3"}>
-        <Text as={"h1"} fontSize={"3xl"}>
+      <div className="mx-auto p-12 max-w-4xl bg-gray-300">
+        <h1 className="text-3xl">
           {values.name}
-        </Text>
-        <Text as={"h2"} fontSize={"xl"}>
+        </h1>
+        <h2 className="text-xl">
           {values.description}
-        </Text>
+        </h2>
         <Link href={"/print"} target={"_blank"}>Printable menu</Link>
-      </Box>
-    </Box>
+      </div>
+    </div>
   )
 }
