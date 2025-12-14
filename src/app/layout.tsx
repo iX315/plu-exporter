f35df5fe2d66b5e62c24d4e068257f424b2c8659
Jury Giannelli <ix315@me.com>
2025-12-14 16:39:22 +0100

    update affected modules (#3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react"
-import { ReactNode } from "react"
 
 import "@/styles/globals.css"
 
-type Props = {
-  children: ReactNode
-}
-
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
-export default function RootLayout({ children }: Props) {
+export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html>
       <body>
