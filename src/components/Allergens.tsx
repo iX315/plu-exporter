3a99f13d5852818fe751c992a56ce0b88b028a08
Jury Giannelli <ix315@me.com>
2026-02-08 13:30:11 +0100

    WIP remove static title

--- src/components/Allergens.tsx
+++ src/components/Allergens.tsx
@@ -3,7 +3,6 @@ import { Fragment } from "react"
 
 export const Allergens = ({ data }: { data: Allergen[] }) => (
   <div>
-    <h3 className="pb-3">Allergens</h3>
     <div className="text-xs grid gap-2 grid-cols-[auto_1fr]">
       {data.map(({ id, name, description }) => (
         <Fragment key={id}>
